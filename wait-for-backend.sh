6334328b21197d89650c8b8f95a22a949c17a987
Franck Royer <franck@coblox.tech>
2018-12-03 14:44:26 +1100

    Remove unneeded log lines

--- wait-for-backend.sh
+++ wait-for-backend.sh
@@ -3,10 +3,6 @@ set -e
 
 cmd="$@"
 
-echo "BACKEND_RPC_USER: $BACKEND_RPC_USER"
-echo "BACKEND_RPC_PASS: $BACKEND_RPC_PASS"
-echo "BACKEND_RPC_HOST: $BACKEND_RPC_HOST"
-
 function check_backend () {
     curl -s --user $BACKEND_RPC_USER:$BACKEND_RPC_PASS --data-binary \
           "{\"jsonrpc\": \"1.0\",\"id\":\"curltest\",\"method\":\"getwalletinfo\", \"params\": [ ]}" -H 'content-type: text/plain;' "http://${BACKEND_RPC_HOST}" \
